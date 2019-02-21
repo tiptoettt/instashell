49b6b4fa9b4eedfc0df0279a3d65e3263c66b57d
The Linux Choice <34893261+thelinuxchoice@users.noreply.github.com>
2019-02-21 18:05:34 -0300

    Update instashell.sh

--- instashell.sh
+++ instashell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Coded by: github.com/thelinuxchoice
-# Instagram: @thelinuxchoice
+# Instagram: @linux_choice
 
 trap 'store;exit 1' 2
 string4=$(openssl rand -hex 32 | cut -c 1-4)
@@ -50,13 +50,13 @@ printf "\e[1;92m ) )| ||  _ \  /___)(_   _)(____ | /___)|  _ \ | ___ || || |    
 printf "\e[1;77m(_/ | || | | ||___ |  | |_ / ___ ||___ || | | || ____|| || |  _____   \e[0m\n"
 printf "\e[1;77m    |_||_| |_|(___/    \__)\_____|(___/ |_| |_||_____) \_)\_)(_____)  \e[0m\n"
 printf "\n"
-printf "\e[1;77m\e[45m  Instagram Brute Forcer v1.5.4 Author: thelinuxchoice (Github/IG)   \e[0m\n"
+printf "\e[1;77m\e[45m  Instagram Brute Forcer v1.5.5 Author: @linux_choice (Github/IG)   \e[0m\n"
 printf "\n"
 }
 
 function start() {
 banner
-checkroot
+#checkroot
 dependencies
 read -p $'\e[1;92mUsername account: \e[0m' user
 checkaccount=$(curl -L -s https://www.instagram.com/$user/ | grep -c "the page may have been removed")
